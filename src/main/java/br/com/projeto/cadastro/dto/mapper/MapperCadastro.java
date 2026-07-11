6aaf847e835467907708a3a6690226da61cd552e
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2026-07-11 20:56:59 -0300

    FEATURE: - MapperDTO: Criado os metodos para response, request e update

--- src/main/java/br/com/projeto/cadastro/dto/mapper/MapperCadastro.java
+++ src/main/java/br/com/projeto/cadastro/dto/mapper/MapperCadastro.java
@@ -1,5 +1,41 @@
 package br.com.projeto.cadastro.dto.mapper;
 
+import br.com.projeto.cadastro.dto.request.CadastroRequest;
+import br.com.projeto.cadastro.dto.response.CadastroResponse;
+import br.com.projeto.cadastro.dto.update.CadastroUpdate;
+import br.com.projeto.cadastro.persistance.entity.Cadastro;
+
 public class MapperCadastro {
+
+    public static CadastroResponse toResponse(Cadastro cadastro) {
+        return CadastroResponse.builder()
+                               .id(cadastro.getId())
+                               .nome(cadastro.getNome())
+                               .email(cadastro.getEmail())
+                               .ativo(cadastro.getAtivo())
+                               .dataCadastro(cadastro.getDataCadastro())
+                               .dataAtualizacao(cadastro.getDataAtualizacao())
+                               .build();
+    }
+
+    public static Cadastro toEntity(CadastroRequest dto) {
+        return Cadastro.builder()
+                       .nome(dto.getNome())
+                       .email(dto.getEmail())
+                       .senha(dto.getSenha())
+                       .build();
+    }
+
+    public static void toUpdate(Cadastro cadastro, CadastroUpdate dto){
+        if(dto.getNome() != null && !dto.getNome().equals(cadastro.getNome())){
+            cadastro.setNome(dto.getNome());
+        }
+
+        if(dto.getEmail() != null && !dto.getEmail().equals(cadastro.getEmail())){
+            cadastro.setEmail(dto.getEmail());
+        }
+
+
+    }
 
 }
