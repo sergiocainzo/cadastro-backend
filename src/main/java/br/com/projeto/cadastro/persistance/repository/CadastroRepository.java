e44b8fbdc5f26799aed7ec910f233d427322107f
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2026-07-11 20:31:00 -0300

    FEATURE: - Repository: Ajustado a interface do repositorio para o JpaRepository

--- src/main/java/br/com/projeto/cadastro/persistance/repository/CadastroRepository.java
+++ src/main/java/br/com/projeto/cadastro/persistance/repository/CadastroRepository.java
@@ -1,5 +1,8 @@
 package br.com.projeto.cadastro.persistance.repository;
 
-public interface CadastroRepository {
+import br.com.projeto.cadastro.persistance.entity.Cadastro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
 
 }
