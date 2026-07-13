22f0350683f4b98414340abe29a681e07f24e173
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2026-07-12 21:20:51 -0300

    FEATURE: - Criados os metodos CRUD do Service

--- src/main/java/br/com/projeto/cadastro/service/CadastroService.java
+++ src/main/java/br/com/projeto/cadastro/service/CadastroService.java
@@ -1,5 +1,24 @@
 package br.com.projeto.cadastro.service;
 
+import br.com.projeto.cadastro.dto.request.CadastroRequest;
+import br.com.projeto.cadastro.dto.response.CadastroResponse;
+import br.com.projeto.cadastro.dto.update.CadastroUpdate;
+import br.com.projeto.cadastro.persistance.entity.Cadastro;
+
+import java.util.List;
+
 public interface CadastroService {
+
+    List<CadastroResponse> getTodosCadastros();
+
+    CadastroResponse setNovoCadastro(CadastroRequest dto);
+
+    CadastroResponse setAtualizarCadastro(Long id, CadastroUpdate dto);
+
+    void setApagarCadastro(Long id);
+
+    void setMudarStatusAtivo(Long id);
+
+    void setAlterarSenha(Long id, String senha);
 
 }
