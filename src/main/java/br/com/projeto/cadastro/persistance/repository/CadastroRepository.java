76e9ad6a441a14282eeeef6a88be9563ab145d5e
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2026-07-12 21:20:30 -0300

    FEATURE: - Criada a querie existsByEmailIgnoreCase(String email) para verificar se o email já existe

--- src/main/java/br/com/projeto/cadastro/persistance/repository/CadastroRepository.java
+++ src/main/java/br/com/projeto/cadastro/persistance/repository/CadastroRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
 
+    boolean existsByEmailIgnoreCase(String email);
+
 }
