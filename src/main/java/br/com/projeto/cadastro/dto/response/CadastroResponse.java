30c163bae2d14df7af9d7bc49dbb7a1fff2a3367
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2026-07-14 15:08:37 -0300

    FIX: - CadastroResponse: Adicionado o atributo senha

--- src/main/java/br/com/projeto/cadastro/dto/response/CadastroResponse.java
+++ src/main/java/br/com/projeto/cadastro/dto/response/CadastroResponse.java
@@ -8,12 +8,13 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
-@JsonPropertyOrder({"id", "nome", "email", "ativo", "dataCadastro", "dataAtualizacao"})
+@JsonPropertyOrder({"id", "nome", "email", "senha","ativo", "dataCadastro", "dataAtualizacao"})
 public class CadastroResponse {
 
     private Long id;
     private String nome;
     private String email;
+    private String senha;
     private Boolean ativo;
     private LocalDateTime dataCadastro;
     private LocalDateTime dataAtualizacao;
